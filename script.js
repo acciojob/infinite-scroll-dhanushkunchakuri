11a629630e0c02642bc0eb48a605d4df06040556
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-21 11:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+let list=document.getElementById("infi-list");
+let count=1;
 
+for(let i=1;i<=10;i++){
+	let ele=document.createElement("li");
+	ele.textContent=`Item ${count++}`;
+	list.appendChild(ele);
+}
+
+window.addEventListener("scroll",()=>{
+	if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+		for(let i=0;i<2;i++){
+			let li=document.createElement("li");
+			li.textContent = "Item"+count++;
+			list.appendChild(i);
+		}
+	}
+	
+});
